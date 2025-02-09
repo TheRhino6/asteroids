bcd9c56714cccf61abeb8d2e9cf7f3d40b76aae2
Rhino <ryanandrewsmedia@gmail.com>
2025-02-09 13:09:36 +0000

    adding sprite class

--- main.py
+++ main.py
@@ -4,13 +4,20 @@
 import pygame
 from constants import *
 
+# This is the main function that runs the game
+
 def main():
+    #initialize the pygame library
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #print ("Starting asteroids!")
-    #print (f"Screen width: {SCREEN_WIDTH}")
-    #print (f"Screen height: {SCREEN_HEIGHT}")
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # This is the main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -19,6 +26,7 @@ def main():
 
         screen.fill(BACKGROUND_COLOUR)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
