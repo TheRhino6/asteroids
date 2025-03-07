cce73004a68a50761362b1227fca614de8d948d9
TheRhino6 <ryanandrewsmedia@gmail.com>
2025-03-07 20:15:56 +0000

    roatation of player

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
 
         screen.fill(BACKGROUND_COLOUR)
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000 # limit frames to 60 FPS
