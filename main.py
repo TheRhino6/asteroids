69cbd392c62933ff8f9f77f7b19d74606b4622a6
TheRhino6 <ryanandrewsmedia@gmail.com>
2025-03-08 09:12:27 +0000

    containers

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
     print ("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    # Create containers
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Assign to containers
+    Player.containers = (updatable, drawable)
+
     dt = 0
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
@@ -14,10 +22,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
 
         screen.fill(BACKGROUND_COLOUR)
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60)/1000 # limit frames to 60 FPS
